d7ef386272e2132ca569b859b4551c92e1f8b59a
Bob D'Errico <bob@bobderrico.com>
2015-08-16 12:30:20 -0400

    Creates function to initialize database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from contextlib import closing
 
 app = Flask(__name__)
 app.config.from_object('config_default')
@@ -7,6 +8,12 @@ app.config.from_object('config_local')
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 if __name__ == '__main__':
     app.run()
